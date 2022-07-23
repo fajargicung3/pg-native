1a99a3d3d5a1b34f024f2dcce3dabaa2e7ae22e4
fajaryuliawan <74240969+fajarys@users.noreply.github.com>
2022-07-23 10:49:56 +0700

    Update edit-proses.php

--- edit-proses.php
+++ edit-proses.php
@@ -6,7 +6,6 @@ $id = $_POST['id'];
 $judul = $_POST['judul'];
 $pengarang = $_POST['pengarang'];
 
-// $q = "UPDATE mining_tb SET mine_result = '$mine_result',mine_color = '$mine_color',install_date = '$date',mine_owner = '$owner' WHERE mine_id = $id";
 
 $update = "UPDATE public.mybooks SET id='$id',judul='$judul',pengarang='$pengarang' WHERE id = '$id'";
 $result = pg_query($conn, $update);
